7beaf3c3853c4c13c8071566fea6a3b2d5816863
Victor Wei <vw1262@gmail.com>
2022-01-29 20:16:50 -0600

    make site look better

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import React from "react";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,31 +14,21 @@ function App() {
         <div className="col-4">
           <nav className="navbar">
             <div className="container-fluid">
-              <Link className="current-link p-2" to="/">home</Link>
+              <Link className="current-link" to="/">home</Link>
               <Link to="about">about</Link>
               <Link to="projects">projects</Link>
             </div>
           </nav>
         </div>
       </div>
-      {/*<div className="row justify-content-center">*/}
-      {/*  <div className="col-2 m-2 text-center">*/}
-      {/*    <h4>Categories</h4>*/}
-      {/*    <ul className="list-group">*/}
-      {/*      <Link to="/musings/technology">technology</Link>*/}
-      {/*      <Link to="/musings/personal">personal</Link>*/}
-      {/*      <Link to="/musings/music">music</Link>*/}
-      {/*      <Link to="/musings/thoughts">fleeting thoughts</Link>*/}
-      {/*    </ul>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
       <div className="row justify-content-center">
         <div className="col-8 m-3">
-          <BlogLink title="hi" date="12/5/21" />
-          <BlogLink title="hi" date="12/5/21" />
-          <BlogLink title="hi" date="12/5/21" />
+          <BlogCard title="hi" date="12/5/21" />
+          <BlogCard title="hi" date="12/5/21" />
+          <BlogCard title="hi" date="12/5/21" />
         </div>
       </div>
+      {/* <Footer /> */}
     </div>
   );
 }
@@ -50,16 +41,13 @@ function App() {
 //   )
 // }
 
-const BlogLink = (props) => {
+const BlogCard = (props) => {
   return (
-    <div className="container m-3 border-top border-bottom">
-      <div className="row d-flex justify-content-between">
-        <div className="col-8">
-          <Link className="h3" to="/blog1">{props.title}</Link>
-        </div>
-        <div className="col-2">
-          <h4>{props.date}</h4>
-        </div>
+    <div class="card m-4">
+      <div class="card-body">
+        <h3 class="card-title">Card title</h3>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text text-muted">January 29 . 6 min</p>
       </div>
     </div>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='container text-center' id="footer">
+      <p>&#169; Made with love and magic</p>
+    </div>
+    
+  )
+}
+
+export default Footer;
